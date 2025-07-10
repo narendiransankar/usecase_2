a8a77b5d864578c159d1f8236af37b64be92e18d
Narendiran Sankar <narendiran.sankar@hcltech.com>
2025-07-10 22:16:00 +0530

    code updated

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "hcl-test-backend-state"
-    key    = "usecase2/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- modules/vpc/output.tf
+++
@@ -1,8 +0,0 @@
-output "vpc_id" {
-  value = aws_vpc.custom_vpc.id
-}
-
-output "public_subnet_ids" {
-  value = aws_subnet.public_subnets[*].id
-}
-
