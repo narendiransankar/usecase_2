0b2d4a2a380fa03a6b19c9e92b404c7563f44578
narendiransankar <narendiransankar1699@gmail.com>
2025-07-10 22:40:36 +0530

    Create variables.tf

---
+++ modules/database/variables.tf
@@ -0,0 +1,36 @@
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "db_sg_id" {
+  description = "Database security group ID"
+  type        = string
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t2.micro"
+}
