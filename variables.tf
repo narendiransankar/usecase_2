b896a83397bdf6bf96beeee73331b72f9bba48d9
narendiransankar <narendiransankar1699@gmail.com>
2025-07-11 10:05:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -66,5 +66,5 @@ variable "instance_type" {
 variable "db_instance_class" {
   description = "RDS instance class"
   type        = string
-  default     = "db.t2.micro"
+  default     = "db.t4g.micro"
 }
