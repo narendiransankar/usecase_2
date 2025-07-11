995ac9fc067b9e9f5a461258b46bee00d531c81a
narendiransankar <narendiransankar1699@gmail.com>
2025-07-11 12:27:39 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,7 +10,7 @@ variable "public_subnet_cidrs" {
   type        = list(string)
   default     = ["10.10.11.0/24","10.10.12.0/24","10.10.13.0/24"]
 }
-variable "private_subnet_cidrs " {
+variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
   default     = ["10.10.14.0/24","10.10.15.0/24","10.10.16.0/24"]
